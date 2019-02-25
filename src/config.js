5c8ee6bbb6f690b4e6dbdc7c68b0f3dd5dba44f9
Matej Minar <matej.minar@brackets.sk>
2019-02-25 16:01:37 +0100

    fire event after saving segment

--- src/config.js
+++ src/config.js
@@ -1,9 +1,21 @@
 export const AUTH_TOKEN = window.Segments.config.AUTH_TOKEN;
 export const CANCEL_PATH = window.Segments.config.CANCEL_PATH;
-export const URL_TABLES_BLUEPRINT = `${window.Segments.config.API_HOST}/api/v1/segments/criteria`;
-export const URL_GET_PAYLOAD = `${window.Segments.config.API_HOST}/api/v1/segments/show`;
-export const URL_POST_PAYLOAD = `${window.Segments.config.API_HOST}/api/v1/segments/detail`;
-export const URL_COUNTER = `${window.Segments.config.API_HOST}/api/v1/segments/counts`;
-export const URL_SUGGESTIONS = `${window.Segments.config.API_HOST}/api/v1/segments/related`;
-export const URL_SEGMENT_CATEGORIES = `${window.Segments.config.API_HOST}/api/v1/segments/groups`;
+export const URL_TABLES_BLUEPRINT = `${
+  window.Segments.config.API_HOST
+}/api/v1/segments/criteria`;
+export const URL_GET_PAYLOAD = `${
+  window.Segments.config.API_HOST
+}/api/v1/segments/show`;
+export const URL_POST_PAYLOAD = `${
+  window.Segments.config.API_HOST
+}/api/v1/segments/detail`;
+export const URL_COUNTER = `${
+  window.Segments.config.API_HOST
+}/api/v1/segments/counts`;
+export const URL_SUGGESTIONS = `${
+  window.Segments.config.API_HOST
+}/api/v1/segments/related`;
+export const URL_SEGMENT_CATEGORIES = `${
+  window.Segments.config.API_HOST
+}/api/v1/segments/groups`;
 export const SEGMENT_ID = window.Segments.config.SEGMENT_ID;
